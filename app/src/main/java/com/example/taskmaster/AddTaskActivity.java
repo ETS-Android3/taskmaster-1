68b0f3795c104bbccf9c1c2a60c113d3bf6623c9
DoaaDaban <doaadaban993@gmail.com>
2021-10-31 22:13:34 +0200

    first sol

---
+++ app/src/main/java/com/example/taskmaster/AddTaskActivity.java
@@ -0,0 +1,30 @@
+package com.example.taskmaster;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class AddTaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_task);
+        getActionBar();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+
+
+        Button button= findViewById(R.id.addTask2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast addTask= Toast.makeText(getApplicationContext(),"new Task added", Toast.LENGTH_SHORT);
+                addTask.show();
+            }
+        });
+    }
+}
--- app/src/main/java/com/example/taskmaster/MainActivity.java
+++ app/src/main/java/com/example/taskmaster/MainActivity.java
@@ -2,13 +2,123 @@ package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity<AppBarConfiguration> extends AppCompatActivity {
+
+    private static final String TAG = "check";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.i(TAG, "onCreate: called");
+
+        Button addTaskButton= findViewById(R.id.addTask);
+        addTaskButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(MainActivity.this, AddTaskActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        Button showAllTasks= findViewById(R.id.allTasks);
+        showAllTasks.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent= new Intent(MainActivity.this, AllTasksActivity.class);
+                startActivity(intent);
+            }
+        });
+
+
+
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.Add_Task) {
+            Intent addTaskPage=new Intent(MainActivity.this,AddTaskActivity.class);
+            startActivity(addTaskPage);
+            return true;
+        }
+
+        if (id == R.id.allMenu) {
+            Intent allTaskPage=new Intent(MainActivity.this,AllTasksActivity.class);
+            startActivity(allTaskPage);
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
+
+
+   // Life cycle of the activities
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+
+        Log.i(TAG, "onStart called: ");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume Called");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause CAllED");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        Log.i(TAG, "CAll OnStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+
+        super.onDestroy();
+
+        Log.i(TAG, "onDestroy: method called");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart: called!");
+    }
+
 }
