f60eb44a1fe434b1952883bcefac8081dd57b224
DoaaDaban <doaadaban993@gmail.com>
2021-11-01 01:08:01 +0200

    solve lab 26

--- app/src/main/java/com/example/taskmaster/AddTaskActivity.java
+++ app/src/main/java/com/example/taskmaster/AddTaskActivity.java
@@ -5,10 +5,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class AddTaskActivity extends AppCompatActivity {
 
+    public static int c =0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,14 +19,17 @@ public class AddTaskActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
-
         Button button= findViewById(R.id.addTask2);
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                TextView textView = findViewById(R.id.textView6);
+                c++;
+                textView.setText("Total Tasks :"+c);
+
                 Toast addTask= Toast.makeText(getApplicationContext(),"new Task added", Toast.LENGTH_SHORT);
                 addTask.show();
-
             }
         });
     }
