47360ca7a7d1d59ec9d5b48741ccccb587941f76
DoaaDaban <doaadaban993@gmail.com>
2021-11-14 16:01:25 +0200

    lab33+34

--- app/src/main/java/com/example/taskmaster/Settings.java
+++ app/src/main/java/com/example/taskmaster/Settings.java
@@ -2,9 +2,14 @@ package com.example.taskmaster;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 public class Settings extends AppCompatActivity {
@@ -14,16 +19,40 @@ public class Settings extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
 
-        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
+        Spinner teamSpin = findViewById(R.id.settingTeamSpin);
 
-            findViewById(R.id.saveId).setOnClickListener(view -> {
-            TextView text = findViewById(R.id.userNameInput);
-            String userName =text.getText().toString();
-
-            editor.putString("userName",userName);
-            editor.apply();
-
+        ArrayAdapter<CharSequence> teamAdapter = ArrayAdapter.createFromResource(this,
+                R.array.teams, android.R.layout.simple_spinner_item);
+        teamAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        teamSpin.setAdapter(teamAdapter);
+        Button button = findViewById(R.id.saveId);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                SharedPreferences preferences2 = PreferenceManager.getDefaultSharedPreferences(Settings.this);
+                SharedPreferences.Editor editor2 = preferences2.edit();
+                TextView text = findViewById(R.id.userNameInput);
+                String team = teamSpin.getSelectedItem().toString();
+                String userName =text.getText().toString();
+                editor2.putString("userName",userName);
+                editor2.putString("team",team);
+                editor2.apply();
+                Intent intent = new Intent(Settings.this,MainActivity.class);
+                startActivity(intent);
+            }
         });
+
+
+//        SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+//        SharedPreferences.Editor editor = sharedPreferences.edit();
+//
+//            findViewById(R.id.saveId).setOnClickListener(view -> {
+//            TextView text = findViewById(R.id.userNameInput);
+//            String userName =text.getText().toString();
+//
+//            editor.putString("userName",userName);
+//            editor.apply();
+//
+//        });
     }
 }
